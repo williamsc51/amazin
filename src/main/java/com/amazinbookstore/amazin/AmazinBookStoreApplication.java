3ce65404a6ae58d0b5c0cdcf3814790c893f6324
Courtney Williams <williams.a.courtney@gmail.com>
2022-12-28 10:43:31 -0500

    Project initiation

---
+++ src/main/java/com/amazinbookstore/amazin/AmazinBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.amazinbookstore.amazin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazinbookstore/amazin/AmazinBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazinbookstore.amazin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazinBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
