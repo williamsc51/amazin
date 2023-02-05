e8bf47e465cfce904092cbd64ab6073d6acc8ec2
Courtney Williams <williams.a.courtney@gmail.com>
2023-02-04 23:11:28 -0500

    disable browser login popup

---
+++ src/main/java/com/amazinbookstore/amazin/config/BasicAuthenticationEntryPointPopupDisable.java
@@ -0,0 +1,18 @@
+package com.amazinbookstore.amazin.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class BasicAuthenticationEntryPointPopupDisable implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
