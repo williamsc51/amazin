8a210c8205824d0150d0bfa6350b9a340d95085c
Courtney Williams <williams.a.courtney@gmail.com>
2023-02-06 23:52:17 -0500

    fetch cart for authenticated user

--- src/main/java/com/amazinbookstore/amazin/controllers/HomeController.java
+++ src/main/java/com/amazinbookstore/amazin/controllers/HomeController.java
@@ -2,14 +2,18 @@ package com.amazinbookstore.amazin.controllers;
 
 import com.amazinbookstore.amazin.dtos.CartDto;
 import com.amazinbookstore.amazin.entities.Book;
+import com.amazinbookstore.amazin.entities.User;
 import com.amazinbookstore.amazin.repositories.BookRepository;
+import com.amazinbookstore.amazin.repositories.UserRepository;
 import com.amazinbookstore.amazin.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -19,6 +23,8 @@ public class HomeController {
 
     @Autowired
     private CartService cartService;
+    @Autowired
+    private UserRepository userRepository;
 
 
     @GetMapping("/")
@@ -31,15 +37,27 @@ public class HomeController {
     }
 
     @GetMapping("/cart")
-    public String getCartItem(Model model, Long userId){
+    public String getCartItem(Model model, Principal principal){
 
-        List<CartDto> cartItems = cartService.getAllCartByUserId(2l);
-        if (cartItems.size() > 0){
-            model.addAttribute("hasBooks", true);
-        }else{
-            model.addAttribute("hasBooks", false);
+        if(principal.getName() != null){
+            Optional<User> user = userRepository.findByEmail(principal.getName());
+
+            if (user.isPresent()){
+                List<CartDto> cartItems = cartService.getAllCartByUserId(user.get().getId());
+
+                if (cartItems.size() > 0){
+                    model.addAttribute("hasBooks", true);
+                    model.addAttribute("cartItems", cartItems);
+                }else{
+                    model.addAttribute("hasBooks", false);
+                }
+
+            }else{
+                return null;
+            }
+        }else {
+            return null;
         }
-        model.addAttribute("cartItems", cartItems);
 
         return "cart";
     }
