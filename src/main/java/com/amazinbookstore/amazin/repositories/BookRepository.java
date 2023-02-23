20f5bd49dff5439e628f72303781c0d74da050ac
Courtney Williams <williams.a.courtney@gmail.com>
2023-02-22 23:04:35 -0500

    add method to Book repo to limit query result

--- src/main/java/com/amazinbookstore/amazin/repositories/BookRepository.java
+++ src/main/java/com/amazinbookstore/amazin/repositories/BookRepository.java
@@ -3,8 +3,12 @@ import com.amazinbookstore.amazin.entities.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource //allow spring data rest to expose this as an endpoint
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findFirst5ByOrderByTitleDesc();
 
         /*
             Spring Data 'JpaRepository' automate CRUD methods to create/read/update/delete a record
